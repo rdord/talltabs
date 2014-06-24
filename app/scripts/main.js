333ae3746bde35acb381fb27c719a3c03e38a48e
Roland <rdorde@gmail.com>
2014-06-24 12:18:23 +0200

    draggable content box

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,26 @@ $(function() {
         fadingSpeed: 100,
         sizingSpeed: 100
     });
+
+    $('.list-wrap').draggable({
+			axis: 'y'
+		});
+
+	$('.list-wrap').bind('drag', function (event, ui) {
+				var gap = ui.position.top + 20;
+
+				if(gap < 10){
+					ui.position.top = -10;
+				}
+				
+				if(gap > 200){
+					ui.position.top = 180;
+				}
+/*
+				$('li a.current').css('padding-bottom', gap);
+*/
+			});
+
+
 });
+
