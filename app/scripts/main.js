3896ffb7f3bafeea3de9dcf2de9737fb6bafb01a
Roland <rdorde@gmail.com>
2014-06-30 14:16:16 +0200

    structure comments

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,14 +10,15 @@ $(function() {
 
     $('.list-wrap').draggable({
 			axis: 'y',
-			containment: [0, 106, 0, 200],
+			containment: [0, 106, 0, 200], /*unlocked: 200 -> 2000*/
 			drag: function( event, ui ) {
 				currentTabHeight = ui.position.top - 90;
 				$('li a.current').height(currentTabHeight);
 				
 			}
 		});
-
+		
+ /* locked */
 	$('.nav li a').click(function() {
 		$('.nav li a').not(this).css('height', '16');
 		$(this).css('height', currentTabHeight);
